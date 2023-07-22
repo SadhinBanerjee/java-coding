78784608d21f2e8fc69499e490d5abf555488ea2
SadhinBanerjee <sadhinbanerjee26@gmail.com>
2023-07-22 11:55:21 +0530

    hello all program

---
+++ secondjavaprogram.java
@@ -0,0 +1,9 @@
+class secondjavaprogram
+{
+    public static void main(String[] args) 
+    {
+        System.out.println("hello all");
+	    System.out.println(args[0]);
+	    System.out.println(args[1]);
+    }
+}
