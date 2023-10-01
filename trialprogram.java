c31d9bc1de7e545784cb9a4838767a60d91f29ea
Sadhin Banerjee <128255949+SadhinBanerjee@users.noreply.github.com>
2023-10-02 00:12:12 +0530

    Delete trialprogram.java

--- trialprogram.java
+++
@@ -1,9 +0,0 @@
-class trialprogram
-{
-    public static void main(String[] args) 
-    {
-        System.out.println("add");
-	    System.out.println(args[0]);
-	    System.out.println(args[1]);
-    }
-}
