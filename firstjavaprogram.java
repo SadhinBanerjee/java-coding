3b0703f32b3aa83cc02e16ea0dcf7b90ee21482e
SadhinBanerjee <sadhinbanerjee26@gmail.com>
2023-11-17 23:57:42 +0530

    Hello java program

---
+++ firstjavaprogram.java
@@ -0,0 +1,9 @@
+public class firstjavaprogram{
+    public static void main(String[] args) {
+        System.out.println("sadhin banerjee");
+
+    //for command form when arguments was pass through command line
+    System.out.println("hello all");
+	    System.out.println(args[0]);
+    }
+}
