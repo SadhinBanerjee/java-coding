87d4b0837cd8a67daa64b397e8b8fdae7559127b
Sadhin Banerjee <128255949+SadhinBanerjee@users.noreply.github.com>
2023-11-17 23:35:28 +0530

    Delete secondjavaprogram.java

--- secondjavaprogram.java
+++
@@ -1,9 +0,0 @@
-class secondjavaprogram
-{
-    public static void main(String[] args) 
-    {
-        System.out.println("hello all");
-	    System.out.println(args[0]);
-	    System.out.println(args[1]);
-    }
-}
